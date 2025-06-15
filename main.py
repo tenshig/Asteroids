9cfe3b3db52490d44c3b17708edc2bdf93881915
tenshig <eagallegos3@gmail.com>
2025-06-15 00:03:32 -0700

    built player to be able to move (and exist)

--- main.py
+++ main.py
@@ -3,20 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
+    # initializing the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('asteroids')
     clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # game loop 
     while True:
+        # exit game when click "x"
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
