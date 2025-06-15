d2be310a9f2a86a5af3022bef6b88072e30eb7a7
tenshig <eagallegos3@gmail.com>
2025-06-14 20:52:17 -0700

    Added the window feature

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('asteroids')
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
